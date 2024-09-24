c2e9f4cbc30f8276859fc7a953fc9c0db77ca7a5
KaykyRenner <kayky.renner.sg@gmail.com>
2024-09-24 17:10:24 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const {app} = require('./server/server')
 
 app.listen(process.env.PORT||3000, ()=>{
-    console.log('app rodando','http://localhost:3333/',process.env.PORT||3000)
+    console.log('app rodando em', `http://localhost:${process.env.PORT || 3000}`);
 })
