aab9b203380f7a24f7b05450eb6c21c07c330b39
KaykyRenner <kayky.renner.sg@gmail.com>
2024-09-25 11:43:35 -0300

    add mongoDB

--- src/banco.js
+++ src/banco.js
@@ -1,9 +1,3 @@
-require('./shared/services/traducoesYup')
-const express = require('express');
-const app = express();
-const router = require('./routes/routes')
-require('dotenv').config()
-
 const mongoose = require('mongoose');
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/meubanco';
@@ -11,11 +5,3 @@ const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/meubanco';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
-
-
-
-app.use(express.json())
-app.use(router)
-
-module.exports = {app};
-
