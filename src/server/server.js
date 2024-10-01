89858bdf84c0f359bbb8ca8def00a14d4b3f47e0
KaykyRenner <kayky.renner.sg@gmail.com>
2024-10-01 10:52:27 -0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -4,16 +4,6 @@ const app = express();
 const router = require('./routes/routes')
 require('dotenv').config()
 
-const mongoose = require('mongoose');
-
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/meubanco';
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
-
-
-
 app.use(express.json())
 app.use(router)
 
