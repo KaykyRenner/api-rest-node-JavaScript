7d13c301c60d511e3cf9a109faacd12e821808f0
KaykyRenner <kayky.renner.sg@gmail.com>
2024-10-07 15:17:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-const {app} = require('./server/server') 
+const { app } = require('./server/server'); 
 const PORT = process.env.PORT || 3000;
-const knex = require('./server/database/bancoDeDados/knexConfig')
-const StartServe = ()=>{app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-});}
-if(process.env.IS_LOCALHOST !== 'true'){
-knex.migrate.latest().then(
-    ()=>{
-        knex.seed.run().then(()=>{StartServe}).catch(console.log('err'))
-    }
-).catch(console.log('erro'))}
-else{
-    StartServe()
+const knex = require('./server/database/bancoDeDados/knexConfig');
+
+const StartServe = () => {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em: http://localhost:${PORT}`);
+    });
+}
+
+if (process.env.IS_LOCALHOST !== 'true') {
+    knex.migrate.latest()
+        .then(() => {
+            return knex.seed.run(); // Retorne a Promise da função de seed
+        })
+        .then(() => {
+            StartServe(); // Aqui você deve chamar a função
+        })
+        .catch((error) => {
+            console.error('Erro durante migrações ou seeds:', error); // Tratamento de erro adequado
+        });
+} else {
+    StartServe();
 }
